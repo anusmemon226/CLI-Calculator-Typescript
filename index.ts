a7904a04bd1f8f9008857787db22d1be06e72c76
anusmemon226 <anusm226@gmail.com>
2024-03-26 01:32:56 +0500

    CLI Calculator Developed

---
+++ index.ts
@@ -0,0 +1,30 @@
+import inquirer from 'inquirer';
+let query = await inquirer.prompt([
+    {
+        message : "Enter First Number : ",
+        type: "number",
+        name: "firstNumber"
+    },
+    {
+        message : "Enter Second Number : ",
+        type: "number",
+        name: "secondNumber"
+    },
+    {
+        message : "Choose Operation : ",
+        type: "list",
+        name: "operator",
+        choices : ["+","-","*","/"]
+    }
+])
+if(query.operator == "+"){
+    console.log(`Answer : ${query.firstNumber + query.secondNumber}`)
+}else if(query.operator == "-"){
+    console.log(`Answer : ${query.firstNumber - query.secondNumber}`)
+}else if(query.operator == "*"){
+    console.log(`Answer : ${query.firstNumber * query.secondNumber}`)
+}else if(query.operator == "/"){
+    console.log(`Answer : ${query.firstNumber / query.secondNumber}`)
+}else{
+    console.log("Invalid Input")
+}
